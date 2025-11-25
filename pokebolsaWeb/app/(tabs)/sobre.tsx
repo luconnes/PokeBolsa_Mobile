f5f07749992010e99e0a77eadf1e3aee5a95e60e
Joao Victor <joao.vr.fernandes@gmail.com>
2025-11-25 00:32:29 -0300

    Sobre nós

---
+++ pokebolsaWeb/app/(tabs)/sobre.tsx
@@ -0,0 +1,23 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+import { ScrollView } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import SobreNosCard from "../components/SobreNosCard";
+
+
+
+export default function SobreScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <SobreNosCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ pokebolsaWeb/app/components/SobreNosCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const SobreNosCard = () => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.titulo}>Porque POKEBOLSA ?</Text>
+      <Text style={styles.texto}>
+        Decidimos replicar o funcionamento de uma bolsa de valores utilizando o funcionamento do mercado de cartas de Pokémon Trading Card Game.
+      </Text>
+      <Image source={require('../assets/logan-paul-wears-most-expensive-pokemon-card-to-wrestlemania_3xwc.jpg')} style={{ width : 250, height: 250}} />
+
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+    padding: 20,
+    margin: 16,
+    elevation: 5, // Sombra no Android
+    shadowColor: '#000', // Sombra no iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    marginBottom: 10,
+    color: '#333',
+  },
+  texto: {
+    fontSize: 16,
+    lineHeight: 22,
+    color: '#555',
+  },
+});
+
+export default SobreNosCard;
